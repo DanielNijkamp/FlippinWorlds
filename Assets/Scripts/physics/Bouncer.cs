93fc7bb1f5623e8dcf55e895d277da2b0a8c54b6
RossTubes <70896941+RossTubes@users.noreply.github.com>
2024-02-29 10:45:00 +0100

    removed redundencies in code

--- Assets/Scripts/physics/Bouncer.cs
+++ Assets/Scripts/physics/Bouncer.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class Bouncer : MonoBehaviour
 {
-    public float explosionStrength = 100f;
-
+    [SerializeField] private float _explosionStrength;
     void OnCollisionEnter(Collision _other)
     {
-      
-        _other.rigidbody.AddExplosionForce(explosionStrength, transform.position, 2f);
-        Debug.Log("test");
+        _other.rigidbody.AddExplosionForce(_explosionStrength, transform.position, 2f);
     }
 }
--- Assets/Scripts/physics/DeathBarrier.cs
+++ Assets/Scripts/physics/DeathBarrier.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class DeathBarrier : MonoBehaviour
 {
-
-
-    public Transform respawnPosition; // Set this in the Unity editor to the desired respawn position
-
+    [SerializeField] private Transform respawnPosition;
     private void OnCollisionEnter(Collision collision)
     {
         {
-            // Check if the colliding object is tagged as "Ball"
             if (collision.gameObject.CompareTag("Ball"))
             {
-                // Move the ball to the respawn position
                 collision.gameObject.transform.position = respawnPosition.position;
             }
         }
