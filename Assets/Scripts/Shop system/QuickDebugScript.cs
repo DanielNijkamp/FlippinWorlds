a229466d821a0e818aba017474867a74c4c25d4b
Kazuma-47 <70643013+Kazuma-47@users.noreply.github.com>
2024-03-12 15:40:49 +0100

    test scene print test

---
+++ Assets/Scripts/Shop system/QuickDebugScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickDebugScript : MonoBehaviour
+{
+    public void printdiNg()
+    {
+        print("BUtton Pressed");
+    }
+}
