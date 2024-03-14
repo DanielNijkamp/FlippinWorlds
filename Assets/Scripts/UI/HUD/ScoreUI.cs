d6805e8d0c5df64b7819dd90c9421e145e0b9de9
Daniel <danielnijkamp2004@gmail.com>
2024-03-14 19:29:24 +0100

    Ingame UI

---
+++ Assets/Scripts/UI/HUD/ScoreUI.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class ScoreUI : MonoBehaviour
+{
+    private TextMeshProUGUI _scoreText;
+    
+    private void Start()
+    {
+        _scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void UpdateUI(int score)
+    {
+        _scoreText.text = score.ToString();
+    }
+}
